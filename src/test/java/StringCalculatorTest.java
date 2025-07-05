8c201d8c8f38709c9265d9a2d525e978b16f984e
valamd <mdvala2107@gamil.com>
2025-07-05 18:11:41 +0530

    Test:add_should_returnZero_whenInputIsEmpty

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,23 @@
+
+package org.manish;
+
+import org.Manish.StringCalculator;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    public void add_should_returnZero_whenInputIsEmpty() {
+        assertEquals(0, calculator.add(""));
+    }
 }
